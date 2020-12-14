8d317c36eb49a0ce358810503ae21b433a746fa6
Olesya Miller <olesyam.miller@gmail.com>
2020-12-14 15:50:11 -0400

    delete comments in the seed file

--- server-side/db/seeds.rb
+++ server-side/db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 cities = File.read('/Users/user/Development/code/portfolio-projects/react-redux-final-project/server-side/db/pr.json')
 
 JSON.parse(cities).each do 
