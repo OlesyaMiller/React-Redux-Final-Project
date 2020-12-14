3ccaf01bf206872392204c7662e265daaa9ad9b0
Olesya Miller <olesyam.miller@gmail.com>
2020-12-14 15:43:39 -0400

    clean up the code

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -12,9 +12,7 @@ const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <Router> gives the component access to router */}
       <App />
-    {/* </Router>  */}
   </Provider>,
   document.getElementById('root')
 );
