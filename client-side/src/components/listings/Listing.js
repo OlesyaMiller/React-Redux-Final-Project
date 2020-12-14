60b3460a7ef916dba563a0f13b42e66660638f18
Olesya Miller <olesyam.miller@gmail.com>
2020-12-14 15:15:33 -0400

    Clean up the code

--- client-side/src/components/listings/Listing.js
+++ client-side/src/components/listings/Listing.js
@@ -6,19 +6,19 @@ const Listing = (props) => {
 
     const [likes, setLikes] = useState(0)
    
-        const { listing: {id, img_url, location, title } } = props  
+    const { listing: {id, img_url, location, title } } = props  
 
-        return (
-            <div>
-                <Link to={`/listings/${id}`}>
-                    <Image style={{ width: '18rem' }} src={img_url} alt={title}/>
-                    <h4>{title}</h4> <strong>{location.name}</strong>
-                    <br/>
-                </Link>
-                <button onClick={() =>setLikes(likes + 1)}>Like</button>
-                <p>Likes: {likes}</p>
-            </div>
-        );
+    return (
+        <div>
+            <Link to={`/listings/${id}`}>
+                <Image style={{ width: '18rem' }} src={img_url} alt={title}/>
+                <h4>{title}</h4> <strong>{location.name}</strong>
+                <br/>
+            </Link>
+            <button onClick={() =>setLikes(likes + 1)}>Like</button>
+            <p>Likes: {likes}</p>
+        </div>
+    )
 }
 
 export default Listing;
