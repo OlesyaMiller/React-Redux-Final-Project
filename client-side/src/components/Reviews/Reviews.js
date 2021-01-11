f1999c77a5bfbafc41627cfa34c69d6193de58d7
Olesya Miller <olesyam.miller@gmail.com>
2021-01-11 12:53:41 -0400

    clean up the code

--- client-side/src/components/Reviews/Reviews.js
+++ client-side/src/components/Reviews/Reviews.js
@@ -5,7 +5,7 @@ class Reviews extends Component {
     render() {
         const {reviews, listingId} = this.props 
 
-        const associatedReviews = reviews.filter(review => review.listing_id === listingId);
+        const associatedReviews = reviews.filter(review => review.listing_id === listingId)
         
         const reviewList = associatedReviews.map((review, index) => {
           return <Review key={index} review={review} />
